fbc65fe57bfb95bcd3d1ac5d04b7353f07668a13
Kaden Barlow <kadenbarlow@gmail.com>
2017-11-15 22:18:41 -0700

    Added angular and creation of stickies

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,91 @@
-angular.module('stickyNotes', [])
-.controller('stickyBoard', [
-  '$scope', '$html',
-  function($scope, $html){
+angular.module('app', [])
+.controller('MainCtrl', [
+  '$scope','$http',
+  function($scope,$http){
 
-    $scope.notes = [];
-  
+    $scope.stickies = [];
+
+    var test = {
+      id: "1",
+      text: "Our Sticky Note Page :D",
+      height: 250,
+      width: 250,
+      top: 10,
+      left: 9
+    };
+
+
+    // Functions on Stickies ------------------------------------------------------------------------------
+
+    $scope.create = function(sticky){
+
+      $('.container').append("<div id=" + sticky.id +"div"+ " class='sticky_note'>"+
+      "<div class='inner_container'>"+
+        "<textarea id=" + sticky.id+"text"+ " class='content'>" + sticky.text + "</textarea>"+
+        "<button id="+sticky.id+"button" + ">Save</button>" + 
+      "</div>");
+
+      $('#'+sticky.id+"button").css({'visibility': 'hidden'});
+      $('#'+sticky.id+"button").click(function(){
+
+        //Call PUT method for text here
+        $('#'+sticky.id+"button").css({'visibility': 'hidden'});
+      });
+      $('#'+sticky.id+"text").click(function(){
+        $('#'+sticky.id+"button").css({'visibility': 'visible'}); // ON Edit enable save button
+      });
+
+      // Set up position and size of sticky
+      $('#'+sticky.id+"div").css({'top': sticky.top, 'left' :sticky.left});
+      $('#'+sticky.id+"div").css({'height': sticky.height, 'width': sticky.width})   
+      $('#'+sticky.id+"div").draggable({
+        handle: "div.inner_container",
+        stop: function(event, ui) {
+            //var Stoppos = $(this).position();
+            //var height = $(this).height();
+            //var width = $(this).width();
+            //alert("Position: " + "\nTop: " + Stoppos.top + "\nLeft: " + Stoppos.left);
+            //alert("Size: " + "\nWidth: " + width + "\nHeight: " + height);
+
+            // Call position save
+        }
+      });
+      
+      
+    };
+
+
+    // Functions accessing Server -----------------------------------------------------------------------------
+    $scope.getStickiesFromServer = function(){
+      return $http.get('/sticky').success(function(data){
+        angular.copy(data, $scope.stickies);
+      });
+    }
+
+    $scope.sendSticky = function(sticky){
+      $http.post('/sticky', sticky).success(function(data){
+        $scope.stickies.push(data);
+      });
+    }
+
+    // $scope.updateText = function(sticky, id){
+    //   $http.put('/sticky/id/text', sticky.text)
+    // }
+
+
+    // Page functions ----------------------------------------------------------------------------------------
+    $scope.getAll = function(){
+      getStickiesFromServer();
+      stickies.each(function(sticky){
+        create(sticky);
+      });
+    }
+
+    $scope.create(test);
+
+    // Run on start up
+    //$scope.getAll();
   }
 ]);
+
+
